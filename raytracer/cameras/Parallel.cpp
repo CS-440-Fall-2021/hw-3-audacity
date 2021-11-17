4e7d9c9ca3e413bc6b6114a298f455d9330af458
Salman Younus <sam.awesome78666@gmail.com>
2021-11-17 23:27:34 +0500

    Added implementations of parallel and perspective classes

---
+++ raytracer/cameras/Parallel.cpp
@@ -0,0 +1,43 @@
+#include "Parallel.hpp"
+
+Parallel::Parallel()
+{
+    dir = Vector3D(0, 0, -1);
+}
+
+Parallel::Parallel(float c)
+{
+    dir = Vector3D(c);
+    dir.normalize();
+}
+
+Parallel::Parallel(float x, float y, float z)
+{
+    dir = Vector3D(x, y, z);
+    dir.normalize();
+}
+
+Parallel::Parallel(const Vector3D &d)
+{
+    dir = Vector3D(d);
+    dir.normalize();
+}
+
+
+Parallel::Parallel(const Parallel &camera)
+{
+    dir = Vector3D(camera.dir);
+}
+
+Parallel &Parallel::operator=(const Parallel &other)
+{
+    dir = Vector3D(other.dir);
+
+    return *this;
+}
+
+
+Vector3D Parallel::get_direction(const Point3D &p) const
+{
+    // Not sure
+}
---
+++ raytracer/cameras/Perspective.cpp
@@ -0,0 +1,40 @@
+#include "Perspective.hpp"
+
+Perspective::Perspective()
+{
+    pos = Point3D();
+}
+
+Perspective::Perspective(float c)
+{
+    pos = Point3D(c);
+}
+
+Perspective::Perspective(float x, float y, float z)
+{
+    pos = Point3D(x, y, z);
+}
+
+Perspective::Perspective(const Point3D &pt)
+{
+    pos = Point3D(pt);
+}
+
+
+Perspective::Perspective(const Perspective &camera)
+{
+    pos = Point3D(camera.pos);
+}
+
+Perspective &Perspective::operator=(const Perspective &other)
+{
+    pos = Point3D(other.pos);
+
+    return *this;
+}
+
+
+Vector3D Perspective::get_direction(const Point3D &p) const
+{
+    // Not sure
+}
