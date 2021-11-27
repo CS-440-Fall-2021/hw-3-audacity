939f759adf069c7de9c0fb47f1c7ad7f99f44d88
Salman Younus <sam.awesome78666@gmail.com>
2021-11-27 23:22:28 +0500

    Fixed a bug in get_rays function

--- raytracer/samplers/Simple.cpp
+++ raytracer/samplers/Simple.cpp
@@ -25,7 +25,7 @@ std::vector<Ray> Simple::get_rays(int px, int py) const
 
     scaled_px = ( (double)px / (double)viewplane_ptr->get_hres() ) * (viewplane_ptr->bottom_right.x - viewplane_ptr->top_left.x);
 
-    scaled_py = ( (double)py / (double)viewplane_ptr->get_vres() ) * (viewplane_ptr->bottom_right.y - viewplane_ptr->top_left.y);
+    scaled_py = ( (double)py / (double)viewplane_ptr->get_vres() ) * (viewplane_ptr->top_left.y - viewplane_ptr->bottom_right.y);
 
     Point3D pixel = viewplane_ptr->top_left + Vector3D(scaled_px, scaled_py, 0);
 
