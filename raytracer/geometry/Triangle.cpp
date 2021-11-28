5d05eb164ca59d51ae1e4a5700e0574d880383aa
Salman Younus <sam.awesome78666@gmail.com>
2021-11-29 00:01:16 +0500

    Removed commented out code

--- raytracer/geometry/Triangle.cpp
+++ raytracer/geometry/Triangle.cpp
@@ -12,15 +12,9 @@ Triangle::Triangle() : Geometry()
 
 Triangle::Triangle(const Point3D &_v0, const Point3D &_v1, const Point3D &_v2) : Geometry()
 {
-  // check if points are not collinear
-  // if (((v1.y - v0.y) * (v2.z - v0.z)) - ((v2.y - v0.y) * (v1.z - v0.z)) != 0)
-  //   if (((v2.x - v0.x) * (v1.z - v0.z)) - ((v1.x - v0.x) * (v2.z - v0.z)) != 0)
-  //     if (((v1.x - v0.x) * (v2.y - v0.y)) - ((v2.x - v0.x) * (v1.y - v0.y)) != 0)
-  //     {
   v0 = Point3D(_v0);
   v1 = Point3D(_v1);
   v2 = Point3D(_v2);
-  // }
 }
 
 Triangle::Triangle(const Triangle &object)
